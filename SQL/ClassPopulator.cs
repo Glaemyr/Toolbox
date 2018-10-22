6c0fed08ffcbfc300e95a0ae5a209be11a928d7c
Casper Boon Beenfeldt Andersen <cbba@energidanmark.dk>
2018-10-22 16:01:05 +0200

    Expanded functionality of the named inserter

--- SQL/ClassPopulator.cs
+++ SQL/ClassPopulator.cs
@@ -1,10 +1,9 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SQL
 {
@@ -15,19 +14,53 @@ namespace SQL
             T returnedObject = Activator.CreateInstance<T>();
             List<PropertyInfo> modelProperties = returnedObject.GetType().GetProperties().OrderBy(p => p.MetadataToken).ToList();
             for (int i = 0; i < modelProperties.Count; i++)
+            {
+                if (!reader.Read()) continue;
                 modelProperties[i].SetValue(returnedObject, Convert.ChangeType(reader.GetValue(i), modelProperties[i].PropertyType), null);
+            }
+
             return returnedObject;
         }
 
         public static T MapToClassByPropertyName<T>(SqlDataReader reader) where T : class
         {
             T returnedObject = Activator.CreateInstance<T>();
-            List<PropertyInfo> modelProperties = returnedObject.GetType().GetProperties().OrderBy(p => p.MetadataToken).ToList();
+            List<PropertyInfo> modelProperties = returnedObject.GetType().GetProperties().ToList();
 
-            foreach (PropertyInfo info in modelProperties)
+            if (!typeof(T).GetInterfaces().Contains(typeof(IEnumerable)))
             {
-                info.SetValue(returnedObject, Convert.ChangeType(reader[info.Name], info.PropertyType)); //TODO: reader[info.Name] can be changed to prioritize a custom tag
+                //If if the type is not an IEnumerable, read 1 row and return that
+                if (!reader.Read()) return null;
+                foreach (PropertyInfo info in modelProperties)
+                {
+                    var conversionType = Nullable.GetUnderlyingType(info.PropertyType) ?? info.PropertyType;
+                    var value = reader[info.Name];
+                    if(value is DBNull) continue;
+                    info.SetValue(returnedObject, Convert.ChangeType(value, conversionType)); //TODO: reader[info.Name] can be changed to prioritize a custom tag
+                }
             }
+            else
+            { // Else: get the first generic argument, make a list of that, add each row as an element, and return it as a T
+                Type elemType = returnedObject.GetType().GetGenericArguments()[0];
+                IList items = (IList) Activator.CreateInstance(typeof(List<>).MakeGenericType(elemType));
+                while (reader.Read())
+                {
+                    var item = Activator.CreateInstance(elemType);
+                    modelProperties = item.GetType().GetProperties().ToList();
+                    foreach (PropertyInfo info in modelProperties)
+                    {
+                        var conversionType = Nullable.GetUnderlyingType(info.PropertyType)?? info.PropertyType;
+                        var value = reader[info.Name];
+                        info.SetValue(item, Convert.ChangeType(value, conversionType)); //TODO: reader[info.Name] can be changed to prioritize a custom tag
+                    }
+
+                    items.Add(item);
+                }
+
+                return (T) items;
+            }
+
+
             return returnedObject;
         }
     }
