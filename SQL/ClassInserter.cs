f1f0371f292cd10c71daf9f622a0695b5866289f
Casper Boon Beenfeldt Andersen <cbba@energidanmark.dk>
2019-01-21 14:26:05 +0100

    Added IEnumerable handling to ClassInserter along with check that inserts does not exceed 1000, as SQL does not allow this.

--- SQL/ClassInserter.cs
+++ SQL/ClassInserter.cs
@@ -1,56 +1,148 @@
 ﻿using System;
+using System.Collections;
 using System.Data.SqlClient;
 using System.Globalization;
 using System.Linq;
 using System.Reflection;
 
+
 namespace Toolbox.SQL
 {
     public class ClassInserter
     {
-        public static void Insert(string connString, object classToInsert,Table tabeSpec = null)
+        public static void Insert(string connString, object classToInsert, Table tabeSpec = null)
         {
             string tableName;
             string schemaName = "dbo";
-            var classType = classToInsert.GetType();
-            if (classType.CustomAttributes.Any(a => a.AttributeType == typeof(Table)))
+            if (!classToInsert.GetType().GetInterfaces().Contains(typeof(IEnumerable)))
             {
-                var schemaAttribute = classType.CustomAttributes.First(a => a.AttributeType == typeof(Table))
-                    .NamedArguments.First(n => n.MemberName == "Schema").TypedValue.Value as string;
-                tableName = classType.CustomAttributes.First(a => a.AttributeType == typeof(Table)).NamedArguments
-                    .First(n => n.MemberName == "TableName").TypedValue.Value as string;
-                if (!string.IsNullOrEmpty(schemaAttribute)) schemaName = schemaAttribute;
-            }
-            else if (tabeSpec != null)
-            {
-                var schemaAttribute = tabeSpec.Schema;
-                tableName = tabeSpec.TableName;
-                if (!string.IsNullOrEmpty(schemaAttribute)) schemaName = schemaAttribute;
+
+
+                var classType = classToInsert.GetType();
+                if (classType.CustomAttributes.Any(a => a.AttributeType == typeof(Table)))
+                {
+                    var schemaAttribute = classType.CustomAttributes.First(a => a.AttributeType == typeof(Table))
+                        .NamedArguments.First(n => n.MemberName == "Schema").TypedValue.Value as string;
+                    tableName = classType.CustomAttributes.First(a => a.AttributeType == typeof(Table)).NamedArguments
+                        .First(n => n.MemberName == "TableName").TypedValue.Value as string;
+                    if (!string.IsNullOrEmpty(schemaAttribute)) schemaName = schemaAttribute;
+                }
+                else if (tabeSpec != null)
+                {
+                    var schemaAttribute = tabeSpec.Schema;
+                    tableName = tabeSpec.TableName;
+                    if (!string.IsNullOrEmpty(schemaAttribute)) schemaName = schemaAttribute;
+                }
+                else
+                    tableName = classType.Name;
+
+                string insertInto = "INSERT INTO " + schemaName + "." + tableName + "(\n";
+                insertInto = insertInto.Replace("\"", "");
+                string values = "VALUES\n(";
+                bool first = true;
+                foreach (var info in classType.GetProperties())
+                {
+                    if (info.CustomAttributes.Any(a => a.AttributeType == typeof(SkipInInserter))) continue;
+                    insertInto += first ? info.Name : "\n," + info.Name;
+                    values += first
+                        ? setQuotes(info, classToInsert)
+                        : "\n," + setQuotes(info, classToInsert).ToString();
+                    first = false;
+                }
+
+                var insertString = insertInto + ")\n" + values + ")";
+
+
+                using (var conn = new SqlConnection(connString))
+                {
+                    var cmd = conn.CreateCommand();
+                    cmd.CommandText = insertString;
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                }
             }
             else
-                tableName = classToInsert.GetType().Name;
-
-            string insertInto = "INSERT INTO " + schemaName + "." + tableName + "(\n";
-            insertInto = insertInto.Replace("\"", "");
-            string values = "VALUES\n(";
-            bool first = true;
-            foreach (var info in classType.GetProperties())
             {
-                if (info.CustomAttributes.Any(a => a.AttributeType == typeof(SkipInInserter))) continue;
-                insertInto += first ? info.Name : "\n," + info.Name;
-                values += first ? setQuotes(info, classToInsert) : "\n," + setQuotes(info, classToInsert).ToString();
-                first = false;
+                TooBigTest((IEnumerable) classToInsert);
+                
+                var classType = classToInsert.GetType().GetGenericArguments()[0];
+                if (classType.CustomAttributes.Any(a => a.AttributeType == typeof(Table)))
+                {
+                    var schemaAttribute = classType.CustomAttributes.First(a => a.AttributeType == typeof(Table))
+                        .NamedArguments.First(n => n.MemberName == "Schema").TypedValue.Value as string;
+                    tableName = classType.CustomAttributes.First(a => a.AttributeType == typeof(Table)).NamedArguments
+                        .First(n => n.MemberName == "TableName").TypedValue.Value as string;
+                    if (!string.IsNullOrEmpty(schemaAttribute)) schemaName = schemaAttribute;
+                }
+                else if (tabeSpec != null)
+                {
+                    var schemaAttribute = tabeSpec.Schema;
+                    tableName = tabeSpec.TableName;
+                    if (!string.IsNullOrEmpty(schemaAttribute)) schemaName = schemaAttribute;
+                }
+                else
+                    tableName = classType.Name;
+
+                string insertInto = "INSERT INTO " + schemaName + "." + tableName + "\n";
+                insertInto = insertInto.Replace("\"", "");
+                string values = "VALUES\n";
+                bool first = true;
+                foreach (var info in classType.GetProperties())
+                {
+                    if (info.CustomAttributes.Any(a => a.AttributeType == typeof(SkipInInserter))) continue;
+                    insertInto += first ? "(" + info.Name : "\n," + info.Name;
+                    first = false;
+
+                }
+
+                var enumer = ((IEnumerable) classToInsert).GetEnumerator();
+                while (enumer.MoveNext())
+                {
+                    
+                    first = true;
+                    foreach (var info in classType.GetProperties())
+                    {
+                        if (info.CustomAttributes.Any(a => a.AttributeType == typeof(SkipInInserter))) continue;
+                        values += first? "("+setQuotes(info, enumer.Current): "\n," + setQuotes(info, enumer.Current);
+                        first = false;
+                    }
+                    values += "),\n";
+                }
+                values = values.TrimEnd(new[] {',', '\n'});
+
+                var insertString = insertInto + ")\n" + values;
+
+
+                using (var conn = new SqlConnection(connString))
+                {
+                    var cmd = conn.CreateCommand();
+                    cmd.CommandText = insertString;
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        private static void TooBigTest(IEnumerable classToInsert)
+        {
+            var cnt = 0;
+            var c = (ICollection) classToInsert;
+            if (c != null)
+            {
+                cnt = c.Count;
+            }
+            else
+            {
+                var enumer = ((IEnumerable)classToInsert).GetEnumerator();
+                while (enumer.MoveNext())
+                {
+                    cnt++;
+                }
             }
 
-            var insertString = insertInto + ")\n" + values + ")";
-
-
-            using (var conn = new SqlConnection(connString))
+            if (cnt>1000)
             {
-                var cmd = conn.CreateCommand();
-                cmd.CommandText = insertString;
-                conn.Open();
-                cmd.ExecuteNonQuery();
+                throw new ArgumentOutOfRangeException($"Tried to insert {cnt} values.\nSQL prohibits inserts of more than a 1000 values. ");
             }
         }
 
